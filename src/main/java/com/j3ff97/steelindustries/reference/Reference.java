83314807807e6523813bd575866f43b2329362d2
J3FF97 <jefri@quicknet.nl>
2015-02-25 15:46:55 +0100

    Added Japanese Language file (courtesy of Cr0wn) Prepared for next version.
    Current errors:
    Textures on titanium block don't go on the right side. (Dev environment only)

--- src/main/java/com/j3ff97/steelindustries/reference/Reference.java
+++ src/main/java/com/j3ff97/steelindustries/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String ID = "SteelIndustries";
     public static final String NAME = "SteelIndustries";
-    public static final String VERSION = "1.7.10-0.6";
+    public static final String VERSION = "1.7.10-0.7";
     public static final String CLIENT_PROXY = "com.j3ff97.steelindustries.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.j3ff97.steelindustries.proxy.ServerProxy";
     public static final String GUIFACTORY = "com.j3ff97.steelindustries.client.gui.GuiFactory";
